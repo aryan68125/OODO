949e9e6133cc0f2a9e5d412651bba21d8fcd53fe
aryan68125 <aryan68125@gmail.com>
2025-02-12 15:49:26 +0530

    Added a boolean field demo in the student module

--- OODOO_version_18/odoo/custom_addons/student/models/models.py
+++ OODOO_version_18/odoo/custom_addons/student/models/models.py
@@ -9,3 +9,4 @@ class Student(models.Model):
     email = fields.Char(string="Email")
     address = fields.Text(string="Student's address",help="Enter student's permanent address here")
     html_field_demo = fields.Html(string="HTML Field Demo")
+    is_agreed = fields.Boolean(string="Do you agree to the terms and consitons?", default="False", help="check to agree with the terms and conditions or else leave it un-checked")
