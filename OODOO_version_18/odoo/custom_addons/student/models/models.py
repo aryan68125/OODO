add92747e7e52b75f0494acfa723a485cd1039d7
aryan68125 <aryan68125@gmail.com>
2025-02-12 17:07:15 +0530

    Added selection field one using static options and another using dynamic options using a method that spits out options for the selection field in student module

--- OODOO_version_18/odoo/custom_addons/student/models/models.py
+++ OODOO_version_18/odoo/custom_addons/student/models/models.py
@@ -10,3 +10,10 @@ class Student(models.Model):
     address = fields.Text(string="Student's address",help="Enter student's permanent address here")
     html_field_demo = fields.Html(string="HTML Field Demo")
     is_agreed = fields.Boolean(string="Do you agree to the terms and consitons?", default="False", help="check to agree with the terms and conditions or else leave it un-checked")
+    gender = fields.Selection(
+        [("Male","Male"),("Female","Female")]
+        )
+    major = fields.Selection("_get_major_list", string="Select Major")
+
+    def _get_major_list(self):
+        return [("CS","Computer Science"),("IT","Information Technology"),("EC","Electronics and Communication"),("ME","Mechanical Engineering"), ("DA","Data Science and Artificial Engineering")]
