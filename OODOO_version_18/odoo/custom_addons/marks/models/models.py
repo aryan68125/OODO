02c7aba2d63a3f4ee0fad99b678eda904bba0489
aryan68125 <aryan68125@gmail.com>
2025-02-14 18:00:54 +0530

    date field , json field , decimal field

--- OODOO_version_18/odoo/custom_addons/marks/models/models.py
+++ OODOO_version_18/odoo/custom_addons/marks/models/models.py
@@ -17,35 +17,6 @@
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
-# from odoo import models, fields, api
-
-# class Marks(models.Model):
-#     _name = 'marks'
-#     _description = 'Student Marks'
-
-#     subject_name = fields.Char(string="Subject name", required=True, index=True)
-#     marks = fields.Integer(string="Marks",default=0, copy=False)
-#     grade = fields.Char(string="Student's grade",compute="_compute_grade", required=False)
-#     student_name = fields.Char(string="Student Name", required=True,readonly=False,translate=True)
-
-#     @api.depends('marks')
-#     def _compute_grade(self):
-#         for record in self:
-#             if record.marks >=91 and record.marks <= 100:
-#                 record.grade = "A+"
-#             elif record.marks >=81 and record.marks <= 90:
-#                 record.grade = "A"
-#             elif record.marks >=71 and record.marks <= 80:
-#                 record.grade = "B"
-#             elif record.marks >=61 and record.marks <= 70:
-#                 record.grade = "C"
-#             elif record.marks >=51 and record.marks <= 60:
-#                 record.grade = "D"
-#             elif record.marks >=41 and record.marks <= 50:
-#                 record.grade = "E"
-#             else:
-#                 record.grade = "F"
-
 from odoo import models, fields, api
 
 class Marks(models.Model):
--- OODOO_version_18/odoo/custom_addons/student/models/models.py
+++ OODOO_version_18/odoo/custom_addons/student/models/models.py
@@ -1,5 +1,5 @@
 from odoo import api, models, fields
-
+from datetime import date
 class Student(models.Model):
     _name = "student"  # Correct model name format
     _description = "Student Model"
@@ -10,6 +10,11 @@ class Student(models.Model):
     address = fields.Text(string="Student's address",help="Enter student's permanent address here")
     html_field_demo = fields.Html(string="HTML Field Demo")
     is_agreed = fields.Boolean(string="Do you agree to the terms and consitons?", default="False", help="check to agree with the terms and conditions or else leave it un-checked")
+    fees = fields.Float(string="Student's Fees",digits=(4,4))
+    school_data = fields.Json(string="School Data")
+    joining_date = fields.Date(string="Joining Date")
+    added_at = fields.Date(string="Added at",compute="compute_added_at")
+
     '''
     The ways to implement selection fields in odoo are:
     '''
@@ -19,10 +24,16 @@ class Student(models.Model):
     gender = fields.Selection(
         selection = [("Male","Male"),("Female","Female")]
         )
-
     '''
     Method 2:
     '''
     major = fields.Selection(selection = "_get_major_list", string="Select Major")
     def _get_major_list(self):
         return [("CS","Computer Science"),("IT","Information Technology"),("EC","Electronics and Communication"),("ME","Mechanical Engineering"), ("DA","Data Science and Artificial Engineering")]
+
+    def json_data_store(self):
+        self.school_data = {'name':self.name, 'roll_no':self.roll}
+    
+    @api.depends('name')
+    def compute_added_at(self):
+        self.added_at = date.today()
