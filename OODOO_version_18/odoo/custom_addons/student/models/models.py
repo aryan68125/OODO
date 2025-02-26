f022481a0139577620aae537b56e8e292d88dac8
aryan68125 <aryan68125@gmail.com>
2025-02-26 17:21:55 +0530

    Updated One2many field in odoo

--- OODOO_version_18/odoo/custom_addons/student/models/models.py
+++ OODOO_version_18/odoo/custom_addons/student/models/models.py
@@ -15,7 +15,7 @@ class Student(models.Model):
     name = fields.Char(string="Student Name", required=True)
     roll = fields.Integer(int="Age")
     email = fields.Char(string="Email")
-    address = fields.Text(string="Student's address",help="Enter student's permanent address here")
+    address = fields.Text(string="Student's address",help="Enter student's permanent address here") 
     html_field_demo = fields.Html(string="HTML Field Demo")
     is_agreed = fields.Boolean(string="Do you agree to the terms and consitons?", default="False", help="check to agree with the terms and conditions or else leave it un-checked")
     fees = fields.Float(string="Student's Fees",digits=(4,4))
